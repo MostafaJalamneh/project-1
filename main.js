977bfd0fceb121422054449e0c7cf74b5e9df6d3
Your Name <you@example.com>
2021-04-23 02:33:07 +0300

    complete main page

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
-console.log(arrowTitle)
-/* START CODE UNDER THIS LINE */
+const sport = document.querySelector('.Sport')
+sport.addEventListener('click', () => {
+    window.open('quiz.html', '_blank');
+})
+
+const scien = document.querySelector('.Scientific')
+scien.addEventListener('click', () => {
+    window.open('quiz.html', '_blank');
+})
+
+const cul = document.querySelector('.Cultural')
+cul.addEventListener('click', () => {
+    window.open('quiz.html', '_blank');
+})
+
+const reg = document.querySelector('.Religious')
+reg.addEventListener('click', () => {
+    window.open('quiz.html', '_blank');
+})
